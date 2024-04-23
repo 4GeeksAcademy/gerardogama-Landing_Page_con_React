1af9618586fc593b6ffc4e7b6d781be50cc4e5fd
gerardogama <153300914+gerardogama@users.noreply.github.com>
2024-04-23 01:48:06 +0000

    completed

---
+++ src/js/component/footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Footer = () => {
+	return (
+    <div className="card text-center">
+		<div className="footer-expand-lg bg-dark text-center text-white py-5">
+    <p>Copyrigth 4geekAcademy Gerardogama Website 2024</p>
+        </div>
+    </div>
+	);
+};
+
+export default Footer;
